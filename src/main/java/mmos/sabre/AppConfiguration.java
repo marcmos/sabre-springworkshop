8c11ad471e90e4809b19e9151625a043103b73d9
Marcin Moskal <mmos@student.agh.edu.pl>
2016-05-21 12:45:04 +0200

    Load initial counter value from properties

--- src/main/java/mmos/sabre/AppConfiguration.java
+++ src/main/java/mmos/sabre/AppConfiguration.java
@@ -1,9 +1,19 @@
 package mmos.sabre;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @ComponentScan(value = "mmos.sabre")
 @EnableAutoConfiguration
 public class AppConfiguration {
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+                new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setLocation(new ClassPathResource("app.properties"));
+        return propertySourcesPlaceholderConfigurer;
+    }
 }
--- src/main/java/mmos/sabre/CounterBean.java
+++ src/main/java/mmos/sabre/CounterBean.java
@@ -1,12 +1,17 @@
 package mmos.sabre;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @Component
-public class CounterBean {
-    private final AtomicLong counts = new AtomicLong();
+public class CounterBean implements InitializingBean {
+    @Value("${counter.initialValue}")
+    private Long initialValue;
+
+    private AtomicLong counts;
 
     public long get() {
         return counts.get();
@@ -14,5 +19,10 @@ public class CounterBean {
 
     public long incrementAndGet() {
         return counts.incrementAndGet();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        counts = new AtomicLong(initialValue);
     }
 }
